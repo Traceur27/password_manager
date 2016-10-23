8a6e070c898252182393bdfec74d95533dc4ebf2
Radek <radek@rzaluska.com>
2016-10-23 15:36:13 +0200

    Add foreign key

--- zpr/password_manager/models.py
+++ zpr/password_manager/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
 class PasswordEntry(models.Model):
+    user = models.ForeignKey(User)
     site = models.CharField(max_length=120)
     password = models.TextField()
 
