170cef02e7a165712426478d1ac70d79839d6610
Radek <radek@rzaluska.com>
2016-11-12 01:35:39 +0100

    Auto create UserExtension when user is created

--- zpr/password_manager/models.py
+++ zpr/password_manager/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cryptopp import passwordXor
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -29,3 +31,9 @@ class UserExtension(models.Model):
             )
     encryption_algorithm = models.CharField(max_length=50, choices=ALGORITHMS,
                                             default='xor')
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserExtension.objects.create(user=instance)
